73d14bdcd9407c6e1fc2b9fb64db806a343737f3
AkuthotaRaghu1202 <akuthotaraghu@gmail.com>
2021-01-16 02:35:30 +0530

    CrossOrigin added in OrderController - to get route from gateway

--- src/main/java/com/ar/javatech/os/api/controller/OrderController.java
+++ src/main/java/com/ar/javatech/os/api/controller/OrderController.java
@@ -2,16 +2,13 @@ package com.ar.javatech.os.api.controller;
 
 import com.ar.javatech.os.api.dto.TransactionRequest;
 import com.ar.javatech.os.api.dto.TransactionResponse;
-import com.ar.javatech.os.api.entity.Order;
 import com.ar.javatech.os.api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin("/*")
 public class OrderController {
 
     @Autowired
@@ -19,10 +16,6 @@ public class OrderController {
 
     @PostMapping("/book-order")
     public TransactionResponse bookOrder(@RequestBody TransactionRequest request){
-
-        orderService.placeOrder(request);
-
         return orderService.placeOrder(request);
     }
-
 }
